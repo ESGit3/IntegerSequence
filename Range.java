ddcfcbeade9b80ee694f1f015cf7e6c6ea6b715c
ESGit3 <ethanshan706@gmail.com>
2020-12-08 00:24:21 -0500

    moved range

---
+++ Range.java
@@ -0,0 +1,29 @@
+import java.util.NoSuchElementException;
+
+public class Range implements IntegerSequence {
+    private int start, end, current;
+    public Range(int rangeStart,  int rangeEnd) {
+        start = rangeStart;
+        end = rangeEnd;
+        current = 0;
+    }
+    public void reset() {
+        current = 0;
+    }
+    public int length() {
+        return (Math.abs(end - start) + 1);
+    }
+    public boolean hasNext() {
+        return (current < Math.abs(end - start));
+    }
+
+    //@throws NoSuchElementException
+    public int next() {
+        if (start + current > end) {
+            throw new NoSuchElementException();
+        } else {
+            current++;
+        }
+        return start + (current - 1);
+    }
+}
